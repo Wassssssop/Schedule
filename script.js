ceed7c071b1a7dcb96b28796c281e4dfd784933f
Wassssssop <162729965+Wassssssop@users.noreply.github.com>
2024-03-09 03:48:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,162 +7,90 @@ const scheduleData = {
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "",
-      "alternative": false
+      "additionalInfo": ""
     },
     {
       "time": "14:40",
-      "week": "all",
-      "course": "Лекція",
-      "title": "Новітня Історія України",
-      "teacher": "Вербовий О.Ф.",
-      "location": "Онлайн",
-      "additionalInfo": "https://meet.google.com/yao-yrwr-heg",
-      "alternative": false
-    },
-    {
-      "time": "16:20",
-      "week": "all",
-      "course": "Лекція",
-      "title": "Історія політики країн Заходу щодо України",
-      "teacher": "Машевський О.П.",
-      "location": "Онлайн",
-      "additionalInfo": "https://knu-ua.zoom.us/j/88035974768?pwd=NWxnZll2YkxiWkowaTZ3Y0lqVTZhdz09",
-      "alternative": false
-    },
-    {
-      "time": "17:55",
-      "week": "all",
-      "course": "Лекція",
-      "title": "Історія раннього Нового часу",
-      "teacher": "Пількевич А. Л.",
-      "location": "Онлайн",
-      "additionalInfo": "https://us02web.zoom.us/j/71399386454?pwd=RitDZGxhUVpWcHFpRkVUMU9ER0J3QT09",
-      "alternative": false
-    },
-    // Америка
-    {
-      "time": "13:05",
       "week": "all",
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "",
-      "alternative": true
-    },
-    {
-      "time": "14:40",
-      "week": "all",
-      "course": "Лекція",
-      "title": "Новітня Історія України",
-      "teacher": "Вербовий О.Ф.",
-      "location": "Онлайн",
-      "additionalInfo": "https://meet.google.com/yao-yrwr-heg",
-      "alternative": true
+      "additionalInfo": ""
     },
     {
       "time": "16:20",
       "week": "all",
-      "course": "Лекція",
-      "title": "Історичні витоки політичної культури американського суспільства",
-      "teacher": "Корнієнко А.Ю.",
-      "location": "Онлайн",
-      "additionalInfo": "https://us06web.zoom.us/j/87293187730?pwd=ZRmcbu2gX4CrXGJU4SnEHYQHuQknax.1Meeting ID: 872 9318 7730Passcode: 076338",
-      "alternative": true
+      "course": "",
+      "title": "",
+      "teacher": "",
+      "location": "",
+      "additionalInfo": ""
     },
     {
       "time": "17:55",
       "week": "all",
-      "course": "Лекція",
-      "title": "Історія раннього Нового часу",
-      "teacher": "Пількевич А. Л.",
-      "location": "Онлайн",
-      "additionalInfo": "https://us02web.zoom.us/j/71399386454?pwd=RitDZGxhUVpWcHFpRkVUMU9ER0J3QT09",
-      "alternative": true
-    }
+      "course": "",
+      "title": "",
+      "teacher": "",
+      "location": "",
+      "additionalInfo": ""
+    },
   ],
   "tuesday": [
     {
       "time": "13:05",
-      "week": "all", 
+      "week": "all",
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "",
-      "alternative": false
+      "additionalInfo": ""
     },
     {
       "time": "14:40",
       "week": "all",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": "",
-      "alternative": false
+      "course": "Лекція",
+      "title": "Міжкультурна комунікація та етикет міжнародної ввічливості в практиці діаспори",
+      "teacher": "Набока С.В.",
+      "location": "435 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "16:20",
-      "week": "all",
-      "course": "Семінар",
-      "title": "Історія раннього Нового часу",
-      "teacher": "Грегуль А. Ю",
-      "location": "453 аудиторія",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    {
-      "time": "17:55",
-      "week": "all",
-      "course": "Семінар",
-      "title": "Історія Східної Європи та Росії",
-      "teacher": "Гуменний С. Л.",
-      "location": "438 аудиторія",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    // Америка
-    {
-      "time": "13:05",
-      "week": "all", 
-      "course": "",
-      "title": "",
-      "teacher": "",
-      "location": "",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    {
-      "time": "14:40",
-      "week": "all",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": "",
-      "alternative": true
+      "week": "upper",
+      "course": "Лекція",
+      "title": "Новітня історія України",
+      "teacher": "Вербовий О.В.",
+      "location": "435 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "16:20",
-      "week": "all",
+      "week": "lower",
       "course": "Семінар",
-      "title": "Історія раннього Нового часу",
-      "teacher": "Грегуль А. Ю",
-      "location": "453 аудиторія",
-      "additionalInfo": "",
-      "alternative": true
+      "title": "Новітня історія України",
+      "teacher": "Шкабко С. І.",
+      "location": "435 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "17:55",
-      "week": "all",
+      "week": "upper",
       "course": "Семінар",
-      "title": "Історія Східної Європи та Росії",
-      "teacher": "Гуменний С. Л.",
-      "location": "438 аудиторія",
-      "additionalInfo": "",
-      "alternative": true
+      "title": "Міжкультурна комунікація та етикет міжнародної ввічливості в практиці діаспори",
+      "teacher": "Набока С.В.",
+      "location": "435 аудиторія",
+      "additionalInfo": ""
+    },    
+    {
+      "time": "17:55",
+      "week": "lower",
+      "course": "Семінар",
+      "title": "Нова історія  країн Західної Європи та Північної Америки (з 15.02 по 21.05)",
+      "teacher": "Кошелєв А.О.",
+      "location": "435 аудиторія",
+      "additionalInfo": ""
     },
   ],
   "wednesday": [
@@ -173,175 +101,109 @@ const scheduleData = {
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "",
-      "alternative": false
+      "additionalInfo": ""
     },
     {
       "time": "14:40",
       "week": "all",
-      "course": "Лекція",
-      "title": "Історія Східної Європи та Росії",
-      "teacher": "Мордвінцев В. М.",
-      "location": "Онлайн; 10:00",
-      "additionalInfo": "https://zoom.us/j/5169999552?pwd=UENVOXlJYm9hcWRQTDFnd3ZMemgzdz09",
-      "alternative": false
+      "course": "Мова",
+      "title": "Англійська мова за професійним спрямуванням",
+      "teacher": "Цепко Т.А.",
+      "location": "435 аудиторія",
+      "additionalInfo": "https://zoom.us/j/5169999552?pwd=UENVOXlJYm9hcWRQTDFnd3ZMemgzdz09"
     },
     {
       "time": "16:20",
       "week": "all",
-      "course": "Семінар",
-      "title": "Історія політичного устрою країн Західної Європи ",
-      "teacher": "Іванов О. Ф.",
-      "location": "451 аудиторія",
-      "additionalInfo": "",
-      "alternative": false
+      "course": "Мова",
+      "title": "Англійська мова за професійним спрямуванням",
+      "teacher": "Цепко Т.А.",
+      "location": "159 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "17:55",
-      "week": "all",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    // Америка
-    {
-      "time": "13:05",
-      "week": "all",
-      "course": "",
-      "title": "",
-      "teacher": "",
-      "location": "",
-      "additionalInfo": "",
-      "alternative": true
-    },
-    {
-      "time": "14:40",
-      "week": "all",
+      "week": "upper",
       "course": "Лекція",
-      "title": "Історія Східної Європи та Росії",
-      "teacher": "Мордвінцев В. М.",
-      "location": "Онлайн; 10:00",
-      "additionalInfo": "https://zoom.us/j/5169999552?pwd=UENVOXlJYm9hcWRQTDFnd3ZMemgzdz09",
-      "alternative": true
-    },
-    {
-      "time": "16:20",
-      "week": "all",
-      "course": "Семінар",
-      "title": "Історія Канади",
+      "title": "Нова історія країн Азії та Африки",
       "teacher": "Сухобокова О.О.",
-      "location": "448 аудиторія",
-      "additionalInfo": "",
-      "alternative": true
+      "location": "450 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "17:55",
-      "week": "all",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": "",
-      "alternative": true
+      "week": "lower",
+      "course": "Семінар",
+      "title": "Нова історія країн Азії та Африки",
+      "teacher": "Сухобокова О.О.",
+      "location": "450 аудиторія.",
+      "additionalInfo": ""
     },
   ],
   "thursday": [
     {
       "time": "13:05",
-      "week": "lower",
-      "course": "Лекція",
-      "title": "Історія політичного устрою країн Західної Європи ",
-      "teacher": "Іванов О. Ф.",
-      "location": "451 аудиторія",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    {
-      "time": "13:05",
-      "week": "upper",
+      "week": "all",
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "",
-      "alternative": false
-    },
+      "additionalInfo": ""
+    },    
     {
       "time": "14:40",
       "week": "upper",
-      "course": "Семінар",
-      "title": "Історія політики країн Заходу щодо України",
-      "teacher": "Машевський О.П.",
-      "location": "460 аудиторія",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    {
-      "time": "16:20",
-      "week": "upper",
-      "course": "Семінар",
-      "title": "Новітня історія України",
-      "teacher": "Шкабко С. І.",
-      "location": "452 аудиторія",
-      "additionalInfo": "",
-      "alternative": false
-    },
-    {
-      "time": "17:55",
-      "week": "all",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": "",
-      "alternative": false
-    },
-     // Америка
-    {
-      "time": "13:05",
-      "week": "lower",
       "course": "Лекція",
-      "title": "Історія Канади",
-      "teacher": "Сухобокова О.О.",
-      "location": "448 аудиторія",
-      "additionalInfo": "",
-      "alternative": true
+      "title": "Українська діаспора та збереження української традиційної спадщини",
+      "teacher": "вакансія",
+      "location": "150 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "14:40",
-      "week": "upper",
+      "week": "lower",
       "course": "Семінар",
-      "course": "Семінар",
-      "title": "Історичні витоки політичної культури американського суспільства",
-      "teacher": "Корнієнко А.Ю.",
-      "location": "448 аудиторія",
-      "additionalInfo": "",
-      "alternative": true
+      "title": "Українська діаспора та збереження української традиційної спадщини",
+      "teacher": "вакансія",
+      "location": "150 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "16:20",
       "week": "upper",
+      "course": "Лекція",
+      "title": " Україна в контексті світової історії",
+      "teacher": "Орлова Т.В.",
+      "location": "425 аудиторія",
+      "additionalInfo": ""
+    },
+    {
+      "time": "16:20",
+      "week": "lower",
       "course": "Семінар",
-      "title": "Новітня історія України",
-      "teacher": "Шкабко С. І.",
-      "location": "452 аудиторія",
-      "additionalInfo": "",
-      "alternative": true
+      "title": " Україна в контексті світової історії",
+      "teacher": "Орлова Т.В.",
+      "location": "425 аудиторія",
+      "additionalInfo": ""
     },
     {
       "time": "17:55",
-      "week": "all",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": "",
-      "alternative": true
+      "week": "upper",
+      "course": "Лекція",
+      "title": "Нова історія  країн Західної Європи та Північної Америки",
+      "teacher": "Машевський О.П.",
+      "location": "150 аудиторія",
+      "additionalInfo": ""
     },
-    
+    {
+      "time": "17:55",
+      "week": "lower",
+      "course": "Семінар",
+      "title": "Нова історія  країн Західної Європи та Північної Америки",
+      "teacher": "Кошелєв А.О.",
+      "location": "150 аудиторія",
+      "additionalInfo": ""
+    },
   ],
   "friday": [
     {
@@ -351,8 +213,7 @@ const scheduleData = {
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "",
-      "alternative": false
+      "additionalInfo": ""
     },
     {
       "time": "14:40",
@@ -361,42 +222,16 @@ const scheduleData = {
       "title": "Дисципліна вільного вибору",
       "teacher": "",
       "location": "Онлайн",
-      "additionalInfo": "https://telegra.ph/Discipl%D1%96na-v%D1%96lnogo-viboru-studenta-03-08",
-      "alternative": false
+      "additionalInfo": "https://telegra.ph/Discipl%D1%96na-v%D1%96lnogo-viboru-studenta-03-08"
     },
-    //
-    {
-      "time": "13:05",
-      "week": "all",
-      "course": "",
-      "title": "",
-      "teacher": "",
-      "location": "",
-      "additionalInfo": "",
-      "alternative": true
-    },
-    {
-      "time": "14:40",
-      "week": "all",
-      "course": "Лекція",
-      "title": "Дисципліна вільного вибору",
-      "teacher": "",
-      "location": "Онлайн",
-      "additionalInfo": "https://telegra.ph/Discipl%D1%96na-v%D1%96lnogo-viboru-studenta-03-08",
-      "alternative": true
-    },
-    
   ],
-
-  };
-
+};
 
 const days = document.querySelectorAll('.day');
 const scheduleContent = document.querySelectorAll('.schedule-content');
 const today = new Date().getDay();
 let currentDay = '';
 let currentWeek = 'upper'; // Set the default week to upper
-let isAlternativeMode = false; // Added mode flag
 const lessonTimes = ["13:05", "14:40", "16:20", "17:55"];
 
 function displaySchedule(day) {
@@ -407,11 +242,12 @@ function displaySchedule(day) {
   const dayData = scheduleData[day];
   if (dayData && today !== 0) {
     dayData.forEach(timeSlotData => {
-      if ((timeSlotData.week === 'all' || timeSlotData.week === currentWeek) && 
-          ((!isAlternativeMode && !timeSlotData.alternative) || (isAlternativeMode && timeSlotData.alternative))) {
-        if (timeSlotData.course) {
+      if (timeSlotData.week === 'all' || timeSlotData.week === currentWeek) {
+        const lessonIndex = lessonTimes.indexOf(timeSlotData.time);
+        if (lessonIndex !== -1 && timeSlotData.course) {
           const classElement = document.createElement('div');
           classElement.classList.add('class');
+          classElement.style.float = 'left';
           const courseTitle = document.createElement('strong');
           courseTitle.textContent = timeSlotData.course;
           classElement.appendChild(courseTitle);
@@ -436,11 +272,7 @@ function displaySchedule(day) {
             additionalInfo.textContent = timeSlotData.additionalInfo;
           }
           classElement.appendChild(additionalInfo);
-          scheduleContent.forEach((content, index) => {
-            if (lessonTimes[index] === timeSlotData.time) {
-              content.appendChild(classElement);
-            }
-          });
+          scheduleContent[lessonIndex].appendChild(classElement);
         }
       }
     });
@@ -474,52 +306,10 @@ document.getElementById('lower-week').addEventListener('click', () => {
   document.getElementById('upper-week').classList.remove('active');
 });
 
-// Event listener for mode selection
-document.getElementById('normal-mode').addEventListener('click', () => {
-  if (isAlternativeMode) {
-    isAlternativeMode = false;
-    displaySchedule(currentDay);
-    document.getElementById('normal-mode').classList.add('active');
-    document.getElementById('alternative-mode').classList.remove('active');
-  }
-});
-
-document.getElementById('alternative-mode').addEventListener('click', () => {
-  if (!isAlternativeMode) {
-    isAlternativeMode = true;
-    displaySchedule(currentDay);
-    document.getElementById('alternative-mode').classList.add('active');
-    document.getElementById('normal-mode').classList.remove('active');
-  }
-});
-
 // Function to reset active day
 const resetActiveDay = () => {
   days.forEach(d => d.classList.remove('active'));
 };
-
-const upperWeekButton = document.getElementById('upper-week');
-const lowerWeekButton = document.getElementById('lower-week');
-
-upperWeekButton.addEventListener('click', () => {
-  if (currentWeek !== 'upper') {
-    currentWeek = 'upper';
-    scheduleContent.forEach(content => content.innerHTML = '');
-    displaySchedule(currentDay);
-    upperWeekButton.classList.add('active');
-    lowerWeekButton.classList.remove('active');
-  }
-});
-
-lowerWeekButton.addEventListener('click', () => {
-  if (currentWeek !== 'lower') {
-    currentWeek = 'lower';
-    scheduleContent.forEach(content => content.innerHTML = '');
-    displaySchedule(currentDay);
-    lowerWeekButton.classList.add('active');
-    upperWeekButton.classList.remove('active');
-  }
-});
 
 // Initialize schedule for today
 if (today !== 0) {
